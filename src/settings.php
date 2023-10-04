5463121c1c00505cf92b41e564de468b89f7b641
Liberland Software Foundation <121721279+libersoft-org@users.noreply.github.com>
2023-10-04 21:16:21 +0200

    Update settings.php

--- src/settings.php
+++ src/settings.php
@@ -2,7 +2,7 @@
  $GLOBALS['product']       = 'Virtian';
  $GLOBALS['version']       = '0.01';
  $GLOBALS['company']       = 'Liberland Software Foundation';
- $GLOBALS['web']           = 'https://virtian.org';
+ $GLOBALS['web']           = 'https://github.com/libersoft-org/virtian';
  $GLOBALS['year-from']     = '2018';
  $GLOBALS['template-path'] = 'templates/default';
  $GLOBALS['iso-path']      = '/iso';
