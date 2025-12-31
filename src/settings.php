06327db94bbe8623f7dd1a1e6efb27d49832a01b
LiberSoft <121721279+libersoft-org@users.noreply.github.com>
2025-12-31 18:15:22 +0100

    Change company name from 'Liberland Software Foundation' to 'LiberSoft'

--- src/settings.php
+++ src/settings.php
@@ -1,7 +1,7 @@
 <?php
  $GLOBALS['product']       = 'Virtian';
  $GLOBALS['version']       = '0.01';
- $GLOBALS['company']       = 'Liberland Software Foundation';
+ $GLOBALS['company']       = 'LiberSoft';
  $GLOBALS['web']           = 'https://github.com/libersoft-org/virtian';
  $GLOBALS['year-from']     = '2018';
  $GLOBALS['template-path'] = 'templates/default';
